8fd4c71a24324a0fab1ed709d54b548da7476b85
chaxyz <chaiithep2546@gmail.com>
2024-08-25 15:38:45 +0700

    test chaxyz

--- src/main/java/com/itbangmodkradankanbanapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itbangmodkradankanbanapi/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.itbangmodkradankanbanapi.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,5 +25,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleItemNotFoundException(ItemNotFoundForUpdateAndDelete ex, HttpServletRequest request) {
         ErrorResponse errorResponse = new  ErrorResponse(Timestamp.from(Instant.now()), HttpStatus.NOT_FOUND.value(),null, ex.getMessage() ,null, request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(Timestamp.from(Instant.now()), HttpStatus.UNAUTHORIZED.value(),HttpStatus.UNAUTHORIZED.getReasonPhrase(), ex.getMessage(),request.getRequestURI(),null);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
